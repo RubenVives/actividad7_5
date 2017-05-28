aa9dd2775ad3bea11f13c756ac6387f123d480cc
Ruben <Ruben@Ruben-PC>
2017-05-28 12:45:38 +0200

    Añadimos HOLA añadimos como estas

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -17,6 +17,8 @@ public class HolaMundo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Hola");
+        System.out.print("Como estas");
+        
     }
     
 }
